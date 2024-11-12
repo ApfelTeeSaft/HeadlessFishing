3c391acc81267394207b4c8d59dba451edaf50da
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2024-11-12 22:53:02 +0100

    i might have cooked

--- HeadlessFishing/dllmain.cpp
+++ HeadlessFishing/dllmain.cpp
@@ -1,19 +1,53 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
-#include "pch.h"
+#include <Windows.h>
+#include <gl/GL.h>
+#include <dbghelp.h>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
-{
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
+typedef HGLRC(WINAPI* wglCreateContext_t)(HDC hdc);
+wglCreateContext_t original_wglCreateContext = nullptr;
+
+HGLRC WINAPI Hooked_wglCreateContext(HDC hdc) {
+    // uhh i guess this can work?
+    return NULL;
+}
+
+void HookIAT() {
+    HMODULE hModule = GetModuleHandle(NULL);
+    if (!hModule) return;
+
+    ULONG size;
+    PIMAGE_IMPORT_DESCRIPTOR importDesc = (PIMAGE_IMPORT_DESCRIPTOR)ImageDirectoryEntryToData(
+        hModule, TRUE, IMAGE_DIRECTORY_ENTRY_IMPORT, &size);
+
+    if (!importDesc) return;
+
+    // find opengl32.dll
+    for (; importDesc->Name; importDesc++) {
+        const char* modName = (const char*)((BYTE*)hModule + importDesc->Name);
+        if (_stricmp(modName, "opengl32.dll") != 0) continue;
+
+        // get the adress?
+        PIMAGE_THUNK_DATA thunkILT = (PIMAGE_THUNK_DATA)((BYTE*)hModule + importDesc->OriginalFirstThunk);
+        PIMAGE_THUNK_DATA thunkIAT = (PIMAGE_THUNK_DATA)((BYTE*)hModule + importDesc->FirstThunk);
+
+        // find wglCreateContext
+        for (; thunkILT->u1.Function; thunkILT++, thunkIAT++) {
+            PROC* funcAddress = (PROC*)&thunkIAT->u1.Function;
+            if (*funcAddress == (PROC)GetProcAddress(GetModuleHandleA("opengl32.dll"), "wglCreateContext")) {
+                // Found wglCreateContext; replace it
+                DWORD oldProtect;
+                VirtualProtect(funcAddress, sizeof(PROC), PAGE_EXECUTE_READWRITE, &oldProtect);
+                *funcAddress = (PROC)Hooked_wglCreateContext;
+                VirtualProtect(funcAddress, sizeof(PROC), oldProtect, &oldProtect);
+                break;
+            }
+        }
         break;
+    }
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+        HookIAT();
     }
     return TRUE;
 }
-
